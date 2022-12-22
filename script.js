d9a5d6818e4a725f65c001516d8bbc0e98cbc2a9
kavyashree h patwardhan <storeimage@gmail.com>
2022-12-22 12:13:36 +0530

    files added

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let input=document.getElementById('input')
+let expression='';
+function solve(e)
+{
+// console.log(e)
+if(e=='ans')
+{
+let result=eval(expression)
+input.value=result
+}
+else if(e==='clr')
+{
+    expression=''
+    input.value=''
+}
+else
+{
+    expression+=e
+    console.log(expression);
+    input.value=expression;
+}
+}
